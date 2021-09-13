dcc6eb4c2b5b4d701efbb22d1eee7fc0ddbc0683
denis pawlak <90202391+exoduspl@users.noreply.github.com>
2021-09-13 17:29:05 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,39 +1,18 @@
-<?php
-echo "ZSŁ<br>";
-echo "ZSŁ<hr>";
-$name="Janusz";
-echo 'Imię:$name<br>';
-echo "Imię $name<br>";
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h3>Poczatek zawartosci strony</h3>
+    <?php
+      include './inny_plik_dolaczanie.php';
+      include_once './inny_plik_dolaczanie.php';
 
+      require './inny_plik_dolaczanie.php';
 
-$calkowita=10;
-$bin=0b1011;
-$oct=0b1011;
-$hex=0x1011;
-
-echo $hex, '<br>';
-
-$x=10.5;
-$prawda=true;
-$falsz=false;
-echo $falsz;
-
-echo <<< ETYKIETA
-<hr>Imię: $name<br>
-ZSŁ<br>
-Poznań<br>
-ETYKIETA;
-
-$text = <<< ETYKIETA
-<hr>Imię: $name<br>
-ZSŁ<br>
-Poznań<br>
-ETYKIETA;
-echo $text;
-
-echo <<< 'ETYKIETA'
-<hr>Imię: $name<br>
-ZSŁ<br>
-Poznań<br>
-ETYKIETA;
-?>
+     ?>
+    <h3>Koniec zawartosci strony</h3>
+  </body>
+</html>
---
+++ inny_plik_dolaczanie.php
@@ -0,0 +1,5 @@
+<?php
+echo "<hr>Zawartosc pliku: "._JD_.'<hr>';
+
+
+ ?>
