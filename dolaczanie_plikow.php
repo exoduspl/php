5f713c7122bf86ea3491a0a02c2062f9e9ef727b
denis pawlak <90202391+exoduspl@users.noreply.github.com>
2021-09-13 17:29:29 +0200

    Add files via upload

---
+++ dolaczanie_plikow.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h3>Poczatek zawartosci strony</h3>
+    <?php
+      include './inny_plik_dolaczanie.php';
+      include_once './inny_plik_dolaczanie.php';
+
+      require './inny_plik_dolaczanie.php';
+
+     ?>
+    <h3>Koniec zawartosci strony</h3>
+  </body>
+</html>
